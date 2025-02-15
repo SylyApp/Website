786267815aed292517561c42e673635420e29fcc
Paul <pjv017+github@icloud.com>
2025-02-15 13:47:54 +0100

    favicon and deleted media files

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Syly',
   description: 'Syly - reactions from friends on memes',
+  icons: {
+    icon: "/favicon.png", 
+    apple: "/favicon.png", 
+  },
 }
 
 export default function RootLayout({ children }) {
