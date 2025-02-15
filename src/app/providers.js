75ee00997c24703de656e6134b60cf71459333aa
Paul <pjv017+github@icloud.com>
2025-02-15 16:46:10 +0100

    testst

--- src/app/providers.js
+++ src/app/providers.js
@@ -7,7 +7,7 @@ import { PostHogProvider } from 'posthog-js/react';
 export default function PHProvider({ children }) {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      posthog.init('phc_1UPki5xNs0OVIH9s9TouchaMmsmh6z4zPImKRvpbzac', {
+      posthog.init(NEXT_PUBLIC_POSTHOG_KEY, {
         api_host: 'https://eu.i.posthog.com'
       });
     }
